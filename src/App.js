fcc44d9d63d276c5751e46385cc8c7c736285ab7
DondonCo <dondon.co19@gmail.com>
2023-11-30 13:02:54 +0800

    refactored code to create custom hook for geolocation

--- src/App.js
+++ src/App.js
@@ -1,40 +1,19 @@
 import { useState } from "react";
-
-function useGeolocation() {}
+import { useGeolocation } from "./useGeolocation";
 
 export default function App() {
-  const [isLoading, setIsLoading] = useState(false);
   const [countClicks, setCountClicks] = useState(0);
-  const [position, setPosition] = useState({});
-  const [error, setError] = useState(null);
 
-  const { lat, lng } = position;
-
-  function getPosition() {
+  const { isLoading, position, error, getPosition } = useGeolocation();
+  function handleClicks() {
     setCountClicks((count) => count + 1);
-
-    if (!navigator.geolocation)
-      return setError("Your browser does not support geolocation");
-
-    setIsLoading(true);
-    navigator.geolocation.getCurrentPosition(
-      (pos) => {
-        setPosition({
-          lat: pos.coords.latitude,
-          lng: pos.coords.longitude,
-        });
-        setIsLoading(false);
-      },
-      (error) => {
-        setError(error.message);
-        setIsLoading(false);
-      }
-    );
+    getPosition();
   }
+  const { lat, lng } = position;
 
   return (
     <div>
-      <button onClick={getPosition} disabled={isLoading}>
+      <button onClick={handleClicks} disabled={isLoading}>
         Get my position
       </button>
 
